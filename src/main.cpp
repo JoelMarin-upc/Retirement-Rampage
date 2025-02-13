259d6019fed0995d049ef7fd95d4676e9bf7ce58
Oscar Jimenez <oscar.jimenez.benitez@estudiantat.upc.edu>
2025-02-13 15:02:49 +0100

    tuberias

--- src/main.cpp
+++ src/main.cpp
@@ -30,27 +30,31 @@ For a C++ project simply rename the file to .cpp and re-run the build script
 
 #include "tubes.cpp"
 
+Tuberias tuberias;
+
 
 int main ()
 {
-	tubes.SpawnTubes();
+	tuberias.SpawnTubes();
 	// Tell the window to use vysnc and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	//InitWindow(1280, 800, "Hello Raylib");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	//Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
 		// drawing
+		tuberias.MoveTubes();
 		BeginDrawing();
+		tuberias.DrawTubes();
 
 		// Setup the backbuffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
@@ -59,7 +63,7 @@ int main ()
 		DrawText("Hello Raylib", 200,200,20,WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		//DrawTexture(wabbit, 400, 200, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
@@ -67,9 +71,14 @@ int main ()
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	//UnloadTexture(wabbit);
 
 	// destory the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
+}
+
+void UpdateGame() {
+	
+
 }
--- src/tubes.cpp
+++ src/tubes.cpp
@@ -1,54 +1,65 @@
 
+class Tuberias {	// utility header for SearchAndSetResourceDir
 
-#include "raylib.h"
+#define MAX_TUBES 100
+#define TUBES_WIDTH 80
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
-
-typedef struct Tubes {
-	Rectangle rec;
-	Color color;
-	bool active;
-} Tubes;
-
-
-static Tubes tubes[MAX_TUBES * 2] = { 0 };
-static Vector2 tubesPos[MAX_TUBES] = { 0 };
-static int tubesSpeedX = 0;
+	typedef struct Tubes {
+		Rectangle rec;
+		Color color;
+		bool active;
+	} Tubes;
 
 
-void SpawnTubes ()
-{
-	tbuesSpeedX = 2;
+	static Tubes tubes[MAX_TUBES * 2] = { 0 };
+	static Vector2 tubesPos[MAX_TUBES] = { 0 };
+	static int tubesSpeedX = 0;
 
-	for (int i = 0; i < MAX_TUBES; i++)
+
+	void SpawnTubes()
 	{
-		tubesPos[i].x = 400 + 280 * i;
-		tubesPos[i].y = -GetRandomValue(0, 120);
+		tbuesSpeedX = 2;
+
+		for (int i = 0; i < MAX_TUBES; i++)
+		{
+			tubesPos[i].x = 400 + 280 * i;
+			tubesPos[i].y = -GetRandomValue(0, 120);
+		}
+
+		for (int i = 0; i < MAX_TUBES * 2; i += 2)
+		{
+			tubes[i].rec.x = tubesPos[i / 2].x;
+			tubes[i].rec.y = tubesPos[i / 2].y;
+			tubes[i].rec.width = TUBES_WIDTH;
+			tubes[i].rec.height = 255;
+
+			tubes[i + 1].rec.x = tubesPos[i / 2].x;
+			tubes[i + 1].rec.y = 600 + tubesPos[i / 2].y - 255;
+			tubes[i + 1].rec.width = TUBES_WIDTH;
+			tubes[i + 1].rec.height = 255;
+
+			//	tubes[i / 2].active = true;
+		}
 	}
 
-	for (int i = 0; i < MAX_TUBES * 2; i += 2)
+	void MoveTubes()
 	{
-		tubes[i].rec.x = tubesPos[i / 2].x;
-		tubes[i].rec.y = tubesPos[i / 2].y;
-		tubes[i].rec.width = TUBES_WIDTH;
-		tubes[i].rec.height = 255;
+		for (int i = 0; i < MAX_TUBES; i++) tubesPos[i].x -= tubesSpeedX;
 
-		tubes[i + 1].rec.x = tubesPos[i / 2].x;
-		tubes[i + 1].rec.y = 600 + tubesPos[i / 2].y - 255;
-		tubes[i + 1].rec.width = TUBES_WIDTH;
-		tubes[i + 1].rec.height = 255;
-
-	//	tubes[i / 2].active = true;
+		for (int i = 0; i < MAX_TUBES * 2; i += 2)
+		{
+			tubes[i].rec.x = tubesPos[i / 2].x;
+			tubes[i + 1].rec.x = tubesPos[i / 2].x;
+		}
 	}
-}
 
-void MoveTubes() 
-{
-	for (int i = 0; i < MAX_TUBES; i++) tubesPos[i].x -= tubesSpeedX;
-
-	for (int i = 0; i < MAX_TUBES * 2; i += 2)
+	void DrawTubes()
 	{
-		tubes[i].rec.x = tubesPos[i / 2].x;
-		tubes[i + 1].rec.x = tubesPos[i / 2].x;
+		for (int i = 0; i < MAX_TUBES; i++)
+		{
+			DrawRectangle(tubes[i * 2].rec.x, tubes[i * 2].rec.y, tubes[i * 2].rec.width, tubes[i * 2].rec.height, GRAY);
+			DrawRectangle(tubes[i * 2 + 1].rec.x, tubes[i * 2 + 1].rec.y, tubes[i * 2 + 1].rec.width, tubes[i * 2 + 1].rec.height, GRAY);
+		}
+
 	}
 }
