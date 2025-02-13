e6bc0c3453a448877d7e7bb560d98808d31be586
Oscar Jimenez <oscar.jimenez.benitez@estudiantat.upc.edu>
2025-02-13 14:19:47 +0100

    tubes

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,17 @@ For a C++ project simply rename the file to .cpp and re-run the build script
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
+typedef struct Tubes {
+	Rectangle rec;
+	Color color;
+	bool active;
+} Tubes;
+
+
+static Tubes tubes[MAX_TUBES * 2] = { 0 };
+static Vector2 tubesPos[MAX_TUBES] = { 0 };
+
+
 int main ()
 {
 	// Tell the window to use vysnc and work on high DPI displays
