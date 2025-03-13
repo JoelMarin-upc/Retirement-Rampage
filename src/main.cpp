e04ef39d3401a7ec20d5425d931b1a188404c2eb
Luigi-box <luying.bao@estudiantat.upc.edu>
2025-03-13 13:42:51 +0100

    hviyvouvoubvolujb

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,88 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-For a C++ project simply rename the file to .cpp and re-run the build script 
-
--- Copyright (c) 2020-2024 Jeffery Myers
---
---This software is provided "as-is", without any express or implied warranty. In no event 
---will the authors be held liable for any damages arising from the use of this software.
-
---Permission is granted to anyone to use this software for any purpose, including commercial 
---applications, and to alter it and redistribute it freely, subject to the following restrictions:
-
---  1. The origin of this software must not be misrepresented; you must not claim that you 
---  wrote the original software. If you use this software in a product, an acknowledgment 
---  in the product documentation would be appreciated but is not required.
---
---  2. Altered source versions must be plainly marked as such, and must not be misrepresented
---  as being the original software.
---
---  3. This notice may not be removed or altered from any source distribution.
-
-*/
-
 #include "raylib.h"
+#include "resource_dir.h"
+#include "MapReader.h"
+#include "GameObject.h"
+#include "Player.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include <memory>
+#include <vector>
+#include <iostream>
 
-int main ()
+//static const int screenWidth = 800;
+//static const int screenHeight = 450;
+//Vector2 playerSize = { 30,30 };
+//Vector2 playerPosition = { screenWidth / 2, screenHeight / 2 };
+//Vector2 centerPosition = { playerPosition.x + (playerSize.x / 2) - (playerAim.crosshairSize.x / 2) ,playerPosition.y + (playerSize.y / 2) - (playerAim.crosshairSize.x / 2) };
+
+std::vector<std::unique_ptr<GameObject>> gameObjects;
+
+void InitPhase();
+void UpdatingPhase();
+void PaintingPhase();
+
+int main()
 {
-	// Tell the window to use vysnc and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    gameObjects = std::vector<std::unique_ptr<GameObject>>();
+    SearchAndSetResourceDir("resources");
 
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+    SetTargetFPS(60);
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+    InitWindow(0, 0, "Game");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
-		BeginDrawing();
+    // MAPA
+    std::string mapName = "map1.txt";
+    std::string path = GetDirectoryPath(mapName.c_str()) + mapName;
+    std::unique_ptr<GameObject> map = std::make_unique<MapReader>(path);
+    MapReader* mapObj = dynamic_cast<MapReader*>(map.get());
+    mapObj->LoadMap(true);
+    gameObjects.push_back(std::move(map));
 
-		// Setup the backbuffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+    // PLAYER 1
+    Vector2 size = { 30, 30 };
+    Vector2 defPos = { 1, 1 };
+    Vector2 pos = defPos;
+    if (mapObj) {
+        std::vector<std::vector<MapTile>> mapMatrix = mapObj->GetMap();
+        for (int i = 0; i < mapMatrix.size() && pos.x == defPos.x && pos.y == defPos.y; i++)
+            for (int j = 0; j < mapMatrix[i].size() && pos.x == defPos.x && pos.y == defPos.y; j++)
+                if (mapMatrix[i][j].tileChar == '1')
+                    pos = mapMatrix[i][j].position;
+        if (pos.x != defPos.x || pos.y != defPos.y) {
+            //pos.x -= size.x;
+            pos.y -= size.y;
+        }
+    }
+    std::unique_ptr<GameObject> player = std::make_unique<Player>(pos, size);
+    gameObjects.push_back(std::move(player));
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+    InitPhase();
+    while (!WindowShouldClose())
+    {
+        UpdatingPhase();
+        PaintingPhase();
+    }
 
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
-	}
+    CloseWindow();
+    return 0;
+}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+void InitPhase() {
+    for (const auto& gameObject : gameObjects) {
+        gameObject->Init();
+    }
+}
 
-	// destory the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
+void UpdatingPhase() {
+    for (const auto& gameObject : gameObjects) {
+        gameObject->Update();
+    }
+}
+
+void PaintingPhase() {
+    BeginDrawing();
+    ClearBackground(SKYBLUE);
+    for (const auto& gameObject : gameObjects) {
+        gameObject->Draw();
+    }
+    EndDrawing();
 }
